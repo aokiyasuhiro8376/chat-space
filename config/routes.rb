df6595e291c899b570e689dfe5ce4fc598a68188
aokiyasuhiro8376 <aokiyasuhiro8376@gmail.com>
2019-11-02 21:02:40 +0900

    1102 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  root "messages#index"
+  root 'messages#index'
+
+  # get 'groups#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
