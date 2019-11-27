2b35e64c0c1e12d7d83112184565a1422c8e960e
aokiyasuhiro8376 <aokiyasuhiro8376@gmail.com>
2019-11-27 22:53:02 +0900

    1127 2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,13 +7,13 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = @group.message.new(message_params)
+    @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_massages_path(@group), notice: 'メッセージが送信されました'
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
-      render :index 
+      render :index
     end
   end
 
