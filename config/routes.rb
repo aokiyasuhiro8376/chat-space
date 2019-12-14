4e9bd0932f7f4797e4339612f15a8bf64ce2176e
aokiyasuhiro8376 <aokiyasuhiro8376@gmail.com>
2019-12-14 17:07:09 +0900

    1214 01

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   # get 'groups#new'
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
